a017cb2142ffc09fe433a73ee5f821e0e7f78b7e
lvlivingston <lvlivingston@gmail.com>
2023-12-11 16:41:29 -0600

    final step before successful logout

--- catcollector/settings.py
+++ catcollector/settings.py
@@ -124,6 +124,8 @@ STATIC_URL = 'static/'
 
 LOGIN_REDIRECT_URL = '/cats/'
 
+LOGOUT_REDIRECT_URL = '/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
