522918d389471561050f80a4abaff76747caa168
lvlivingston <lvlivingston@gmail.com>
2023-12-11 13:52:23 -0600

    aws setup and add photo url

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
   path('cats/<int:pk>/update/', views.CatUpdate.as_view(), name='cats_update'),
   path('cats/<int:pk>/delete/', views.CatDelete.as_view(), name='cats_delete'),
   path('cats/<int:cat_id>/add_feeding/', views.add_feeding, name='add_feeding'),
+  path('cats/<int:cat_id>/add_photo/', views.add_photo, name='add_photo'),
   path('cats/<int:cat_id>/assoc_toy/<int:toy_id>/', views.assoc_toy, name='assoc_toy'),
   path('cats/<int:cat_id>/unassoc_toy/<int:toy_id>/', views.unassoc_toy, name='unassoc_toy'),
   path('toys/', views.ToyList.as_view(), name='toys_index'),
